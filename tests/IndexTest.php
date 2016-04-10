8e2a5767749004f6e3d214efd3d6150acb5f4d86
Dmitry Krokhin <nekufa@gmail.com>
2016-04-10 12:51:39 +0300

    move miltiple index test

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Tarantool\Mapper\Migrations\Migrator;
+
 class IndexTest extends PHPUnit_Framework_TestCase
 {
     public function testSimple()
@@ -32,5 +34,71 @@ class IndexTest extends PHPUnit_Framework_TestCase
         $unitParam = $manager->get('unit_param');
         $this->assertSame($unitParam->findOne(['unit' => '1', 'param' => '2'])->value, '12');
         $this->assertSame($unitParam->findOne(['unit' => '2', 'param' => '1'])->value, '21');
+    }
+
+    public function testMiltiple()
+    {
+        $manager = Helper::createManager();
+
+        $migrator = new Migrator();
+        $migrator->registerMigration(CreatePosts::class);
+        $migrator->migrate($manager);
+
+        $manager->save($manager->get('posts')->create([
+            'title' => 'a',
+            'slug' => 'a',
+            'body' => 'a',
+            'author' => 'Dmitry Krokhin',
+            'month' => 'March',
+        ]));
+
+        $manager->save($manager->get('posts')->create([
+            'title' => 'b',
+            'slug' => 'b',
+            'body' => 'b',
+            'author' => 'Dmitry Krokhin',
+            'month' => 'March',
+        ]));
+
+        $manager->save($manager->get('posts')->create([
+            'title' => 'c',
+            'slug' => 'c',
+            'body' => 'c',
+            'author' => 'Dmitry Krokhin',
+            'month' => 'January',
+        ]));
+
+        $this->assertCount(3, $manager->get('posts')->find([]));
+
+        // find method
+        $posts = $manager->get('posts')->find(['month' => 'March', 'author' => 'Dmitry Krokhin']);
+        $this->assertCount(2, $posts);
+
+        // swap arguments
+        $posts = $manager->get('posts')->find(['author' => 'Dmitry Krokhin', 'month' => 'January']);
+        $this->assertCount(1, $posts);
+        $this->assertSame('c', $posts[0]->title);
+
+        // magic methods
+        $posts = $manager->get('posts')->byAuthorAndMonth('Dmitry Krokhin', 'March');
+        $this->assertCount(2, $posts);
+
+        $posts = $manager->get('posts')->byMonthAndAuthor('January', 'Dmitry Krokhin');
+        $this->assertCount(1, $posts);
+
+        // skip body
+        $emptyPost = $manager->save($manager->get('posts')->create([
+            'slug' => 'a-post-without-title-and-body',
+            'author' => 'Dmitry Krokhin',
+            'month' => 'December',
+        ]));
+
+        $posts = $manager->get('posts')->byMonthAndAuthor('January', 'Vasiliy');
+        $this->assertCount(0, $posts);
+
+        $newManager = Helper::createManager(false);
+        $newEmptyPost = $newManager->get('posts')->find($emptyPost->id);
+        $this->assertNull($newEmptyPost->header);
+        $this->assertNull($newEmptyPost->body);
     }
 }
