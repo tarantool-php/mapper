14b46a424d7442a1670cc474424dc2cb967186a7
Dmitry Krokhin <nekufa@gmail.com>
2017-03-07 16:34:18 +0300

    remove unimplemented plugin hook

--- src/Plugin.php
+++ src/Plugin.php
@@ -14,5 +14,4 @@ abstract class Plugin
     public function getRepositoryClass(Space $space) {}
     public function getEntityClass(Space $space) {}
     public function beforeCreate(Entity $instance, Space $space) {}
-    public function beforeUpdate(Entity $instance, Space $space) {}
 }
