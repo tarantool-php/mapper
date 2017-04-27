43ff2c3d24bb9930741513e21ab72abd4ff3a4ae
Dmitry Krokhin <nekufa@gmail.com>
2017-04-27 11:19:53 +0300

    implement spy plugin

---
+++ src/Plugins/Spy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tarantool\Mapper\Plugins;
+
+use Tarantool\Mapper\Entity;
+use Tarantool\Mapper\Plugin;
+use Tarantool\Mapper\Space;
+
+class Spy extends Plugin
+{
+    private $create = [];
+    private $update = [];
+    private $remove = [];
+
+    public function beforeCreate(Entity $instance, Space $space)
+    {
+        $this->create[$this->getKey($instance, $space)] = $instance;
+    }
+
+    public function beforeUpdate(Entity $instance, Space $space)
+    {
+        $key = $this->getKey($instance, $space);
+        if(!array_key_exists($key, $this->create)) {
+            $this->update[$key] = $instance;
+        }
+    }
+
+    public function beforeRemove(Entity $instance, Space $space)
+    {
+        $key = $this->getKey($instance, $space);
+
+        if(array_key_exists($key, $this->create)) {
+            unset($this->create[$key]);
+            return;
+        }
+
+        if(array_key_exists($key, $this->update)) {
+            unset($this->update[$key]);
+        }
+
+        $this->remove[$key] = $instance;
+    }
+
+    public function reset()
+    {
+        $this->create = [];
+        $this->update = [];
+        $this->remove = [];
+    }
+
+    private function getKey(Entity $instance, Space $space)
+    {
+        $key = [$space->getName()];
+
+        $format = $space->getFormat();
+        foreach($space->getPrimaryIndex()->parts as $part) {
+            $key[] = $instance->{$format[$part[0]]['name']};
+        }
+
+        return implode(':', $key);
+    }
+
+    public function getChanges() {
+        return (object) get_object_vars($this);
+    }
+}
---
+++ tests/SpyTest.php
@@ -0,0 +1,99 @@
+<?php
+
+use Tarantool\Mapper\Space;
+use Tarantool\Mapper\Plugins\Spy;
+use Tarantool\Mapper\Plugins\Sequence;
+
+class SpyTest extends TestCase
+{
+    public function test()
+    {
+        $mapper = $this->createMapper();
+        $mapper->addPlugin(Sequence::class);
+        $this->clean($mapper);
+
+        $this->assertCount(1, $mapper->getPlugins());
+
+        $person = $mapper->getSchema()->createSpace('person');
+        $person->addProperty('id', 'unsigned');
+        $person->addProperty('email', 'str');
+        $person->createIndex('id');
+
+        $nekufa = $mapper->create('person', ['email' => 'nekufa@gmail.com']);
+
+        $mapper->addPlugin(Spy::class);
+        $this->assertCount(2, $mapper->getPlugins());
+
+        $rybakit = $mapper->create('person', ['email' => 'gen.work@gmail.com']);
+        $nekufa->email = 'dmitry.krokhin@basis.company';
+        $mapper->save($nekufa);
+
+        // there should be 2 changes - 1 create, 1 update
+
+        $spy = $mapper->getPlugin(Spy::class);
+        $this->assertInstanceOf(Spy::class, $spy);
+
+        $changes = $spy->getChanges();
+        $this->assertCount(1, $changes->create);
+        $this->assertCount(1, $changes->update);
+        $this->assertCount(0, $changes->remove);
+
+        $mapper->remove($rybakit);
+        $mapper->remove($nekufa);
+
+        $changes = $spy->getChanges();
+        $this->assertCount(0, $changes->create);
+        $this->assertCount(0, $changes->update);
+        $this->assertCount(1, $changes->remove);
+        $this->assertSame(array_values($changes->remove)[0]->id, $nekufa->id);
+
+        $spy->reset();
+        $changes = $spy->getChanges();
+        $this->assertCount(0, $changes->create);
+        $this->assertCount(0, $changes->update);
+        $this->assertCount(0, $changes->remove);
+
+        $vasya = $mapper->create('person', ['email' => 'vasya@mail.ru']);
+
+        $changes = $spy->getChanges();
+        $this->assertCount(1, $changes->create);
+        $this->assertCount(0, $changes->update);
+        $this->assertCount(0, $changes->remove);
+
+        $this->assertSame([$vasya], array_values($changes->create));
+
+        $vasya->email = 'vasya@ya.ru';
+        $mapper->save($vasya);
+
+        $changes = $spy->getChanges();
+        $this->assertCount(1, $changes->create);
+        $this->assertCount(0, $changes->update);
+        $this->assertCount(0, $changes->remove);
+        $this->assertSame([$vasya], array_values($changes->create));
+
+        $mapper->remove($vasya);
+        $changes = $spy->getChanges();
+        $this->assertCount(0, $changes->create);
+        $this->assertCount(0, $changes->update);
+        $this->assertCount(0, $changes->remove);
+
+        $vasya = $mapper->create('person', ['email' => 'vasya@mail.ru']);
+        $spy->reset();
+
+        $vasya->email = 'vasya@gmail.com';
+        $mapper->save($vasya);
+
+        $changes = $spy->getChanges();
+
+        $this->assertCount(0, $changes->create);
+        $this->assertCount(1, $changes->update);
+        $this->assertCount(0, $changes->remove);
+
+        $mapper->remove($vasya);
+
+        $changes = $spy->getChanges();
+        $this->assertCount(0, $changes->create);
+        $this->assertCount(0, $changes->update);
+        $this->assertCount(1, $changes->remove);
+    }
+}
